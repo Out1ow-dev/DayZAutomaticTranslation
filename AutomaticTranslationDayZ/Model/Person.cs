25463d33bc7ba8ea1148f07c33ec76702bb64f06
илья ковалев <kovi-i@mail.ru>
2024-09-27 23:22:50 +0300

    Добавьте файлы проекта.

---
+++ AutomaticTranslationDayZ/Model/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticTranslationDayZ.Model
+{
+    public class Person
+    {
+        public string Variable { get; set; }
+
+        public string OriginalWord { get; set; }
+    }
+}
---
+++ AutomaticTranslationDayZ/ViewModel/MainViewModel.cs
@@ -0,0 +1,102 @@
+﻿using AutomaticTranslationDayZ.Model;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace AutomaticTranslationDayZ.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        private ObservableCollection<Person> _myCollection;
+        private Person _selectedPerson;
+
+        private string _variablename;
+        private string _originalword;
+
+        public ObservableCollection<Person> MyCollection
+        {
+            get { return _myCollection; }
+            set
+            {
+                _myCollection = value;
+                OnPropertyChanged(nameof(MyCollection));
+            }
+        }
+
+        public Person SelectedPerson 
+        {
+            get { return _selectedPerson; }
+            set
+            {
+                _selectedPerson = value;
+                OnPropertyChanged(nameof(SelectedPerson));
+            }
+        }
+
+        public string VariableName
+        {
+            get
+            {
+                return _variablename;
+            }
+            set 
+            {
+                SetProperty(ref _variablename, value);
+            }
+        }
+
+        public string OriginalWord
+        {
+            get
+            {
+                return _originalword;
+            }
+            set
+            {
+                SetProperty(ref _originalword, value);
+            }
+        }
+
+        public MainViewModel()
+        {
+            MyCollection = new ObservableCollection<Person>();
+        }
+
+        public ICommand DeleteButton
+        {
+            get
+            {
+                return new RelayCommand(() => MyCollection.Remove(SelectedPerson));
+            }
+        }
+
+        public ICommand AddButton
+        {
+            get
+            {
+                
+                return new RelayCommand(() => AddNewElement());
+            }
+        }
+
+        private void AddNewElement()
+        {
+            foreach (var person in MyCollection)
+            {
+                if (person.Variable == VariableName)
+                {
+                    MessageBox.Show("Данная переменная уже существует");
+                    return;
+                }
+            }
+            MyCollection.Add(new Person { Variable = VariableName, OriginalWord = OriginalWord });
+        }
+    }
+
+}
